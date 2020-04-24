cac27d4b12877bb2f5203822d584e976ed06a939
Lenovo <dsridhar45@gmail.com>
2020-04-24 13:32:25 +0530

    push

---
+++ src/test/java/com/example/TestAppHeroku/TestAppHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestAppHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
