9d99c9dbd2a12469367976172c236f3d942bb938
Lenovo <dsridhar45@gmail.com>
2020-04-24 13:22:15 +0530

    push

---
+++ java/com/example/TestAppHeroku/TestAppHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestAppHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppHerokuApplication.class, args);
+	}
+
+}
